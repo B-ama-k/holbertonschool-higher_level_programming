8dc8cede51d5b6d87dde70cf8ee47dc862d52135
B-ama-k <Benjamin.fln74@gmail.com>
2026-01-16 13:24:52 +0000

    ex 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,15 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-
-    argc = len(sys.argv) - 1
-
-    if argc == 0:
-        print("{} arguments.".format(argc))
-    elif argc == 1:
-        print("{} argument:".format(argc))
-    else:
-        print("{} arguments:".format(argc))
-
-    for i, arg in enumerate(sys.argv[1:], 1):
-        print("{}: {}".format(i, arg))
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
